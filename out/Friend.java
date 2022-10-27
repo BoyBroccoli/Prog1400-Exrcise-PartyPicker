8cb2fe40bf8da869c0dd67d1f904383cd4b95aed
Leona Esther <leonaesther@pm.me>
2022-10-27 17:01:40 -0300

    Updated Friend Class

--- out/Friend.java
+++ out/Friend.java
@@ -5,5 +5,8 @@ import java.util.ArrayList;
 public class Friend extends Person{
     ArrayList<String> foodToBring = new ArrayList<String>();
 
-    
+    Friend(String fName, String lName, boolean isInvited, ArrayList<String> food){
+        super(fName, lName, isInvited);
+        this.foodToBring=food;
+    }
 }
